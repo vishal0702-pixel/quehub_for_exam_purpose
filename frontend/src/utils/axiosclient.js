db3936908400a7a099049511565b545d50d7cf71
vishal <vishalcodex07@gmail.com>
2025-12-06 16:21:36 +0530

    updated

--- frontend/src/utils/axiosclient.js
+++ frontend/src/utils/axiosclient.js
@@ -1,11 +1,10 @@
+// utils/axiosclient.js
 import axios from "axios";
 
 const axiosclient = axios.create({
-  baseURL: "https://quehub-for-exam-purpose-2001.onrender.com/api",
+  baseURL: "https://quehub-for-exam-purpose-2001.onrender.com/api", // /api is important
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
 
 export default axiosclient;
